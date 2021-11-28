7eabe6efdbbc84e144e791d653eee168cdc71acc
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2021-11-29 01:10:53 +0900

    Test: e2e 테스트 작성
    1. e2e 테스트 작성
    - 회원 가입 ~ 타이어 정보 조회까지 작성

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,86 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "./../src/app.module";
+import { getConnection } from "typeorm";
+import { TrimOverlapException } from "src/domain/trim/exception/TrimOverlapException";
+import { UserOverlapException } from "src/domain/user/exception/UserOverlapException";
+import { NotFoundUserTrimException } from "src/domain/trim/exception/NotFoundUserTrimException";
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe("AppController (e2e)", () => {
+	let app: INestApplication;
+	let token;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+	beforeEach(async () => {
+		jest.setTimeout(30000);
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule]
+		}).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+	afterAll(async () => {
+		await getConnection().close();
+	});
+
+	it("/user/signup (Post)", () => {
+		try {
+			return request(app.getHttpServer()).post("/user/signup").send({
+				id: "testid",
+				password: "testPw"
+			});
+		} catch (e) {
+			expect(e).toEqual(UserOverlapException);
+		}
+	});
+
+	it("/user/signin (Post)", () => {
+		return request(app.getHttpServer())
+			.post("/user/signin")
+			.send({
+				id: "testid",
+				password: "testPw"
+			})
+			.expect(201)
+			.expect((res) => {
+				token = res.body.token;
+				expect(typeof token).toBe("string");
+			});
+	});
+
+	it("/trim (Post)", () => {
+		try {
+			return request(app.getHttpServer())
+				.post("/trim")
+				.send([
+					{
+						id: "testid",
+						trimId: 5000
+					}
+				])
+				.expect(201)
+				.expect((res) => {
+					expect(typeof res.body).toBe("object");
+				});
+		} catch (e) {
+			expect(e).toEqual(TrimOverlapException);
+		}
+	});
+
+	it("/tire (Get)", () => {
+		try {
+			return request(app.getHttpServer())
+				.get("/tire?trimId=5000")
+				.set("Authorization", `Bearer ${token}`)
+				.expect(200)
+				.expect((res) => {
+					expect(typeof res.body).toBe("object");
+					expect(res.body).toHaveLength(2);
+				});
+		} catch (e) {
+			expect(e).toEqual(NotFoundUserTrimException);
+		}
+	});
 });
